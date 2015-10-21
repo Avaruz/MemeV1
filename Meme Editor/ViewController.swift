7d7fd2a930ccabe87c5bf0e018d233439fab96d7
Adhemar <adhemar.sgv@gmail.com>
2015-10-21 13:50:59 -0400

    Initial Commit

---
+++ Meme Editor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meme Editor
+//
+//  Created by Adhemar Soria Galvarro on 21/10/15.
+//  Copyright © 2015 Adhemar Soria Galvarro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
